2e0e0b54533864b75b699b53a8a69b0ef5251f61
Andrew Durance <adurance@localhost.localdomain>
2021-09-10 08:34:17 -0700

    lab9 changes

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html> 
 	<head> 
-	<title>PHP Test</title>
+	<title>PHP Test from Lab9</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>A New Example of PHP in Action</b></p>
+		<p><b>A New Example of PHP in Action!!!!</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	</body> 
